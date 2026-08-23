298928ea68770aaa8104bd666c4d60e06a8c1fc1
Ngo Thanh Tai <ngothanhtai12345a@gmail.com>
2026-08-23 19:20:48 +0700

    add resource

---
+++ Command.sh
@@ -0,0 +1,8 @@
+
+#Start docker compose
+docker-compose -f docker-compose-load-balance.yaml up -d
+#[Optional] Force rebuild dockerfile.
+docker-compose -f docker-compose-load-balance.yaml up -d --build
+#Terminate all resource
+docker-compose -f docker-compose-load-balance.yaml down
+
